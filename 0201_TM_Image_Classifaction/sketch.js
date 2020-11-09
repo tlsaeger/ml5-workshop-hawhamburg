731e426ccfce764fc0e27c59891cddb805e284d8
Tom-Lucas Säger <tlsaeger@icloud.com>
2020-11-09 11:27:06 +0100

    added example images

--- 0201_TM_Image_Classifaction/sketch.js
+++ 0201_TM_Image_Classifaction/sketch.js
@@ -1,7 +1,36 @@
-function setup() {
-  createCanvas(400, 400);
+// Initialize the Image Classifier method with MobileNet. A callback needs to be passed.
+let classifier;
+// A variable to hold the image we want to classify
+let img;
+
+let modelUrl = "https://teachablemachine.withgoogle.com/models/BssWL1D7m/";
+
+function preload() {
+  classifier = ml5.imageClassifier( modelUrl + 'model.json', modelReady);
+  img = loadImage('images/Tommy.png');
+  console.log(classifier);
 }
 
-function draw() {
-  background(220);
+function setup() {
+  createCanvas(400, 400);
+  classifier.classify(img, gotResult);
+  image(img, 0, 0,400,400);
+}
+
+function modelReady(){
+  createDiv("Model ist geladen")
+  console.log("Model Geladen");
+}
+
+// A function to run when we get any errors and the results
+function gotResult(error, results) {
+  // Display error in the console
+  if (error) {
+    console.error(error);
+  }
+  // The results are in an array ordered by confidence.
+  console.log(results);
+  createDiv('Label: ' + results[0].label);
+  createDiv('Confidence: ' + results[0].confidence);
+  
 }
