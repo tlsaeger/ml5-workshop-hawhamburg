b5f97d22eef4a7a7f8a0ee58a7ef3c59df77efe7
Tom-Lucas Säger <tlsaeger@icloud.com>
2020-11-10 15:35:50 +0100

    fixes

--- 0202_poseNet-Maske/sketch.js
+++ 0202_poseNet-Maske/sketch.js
@@ -1,7 +1,56 @@
-function setup() {
-  createCanvas(400, 400);
+/* ml5.js Workshop 
+HAW Hamburg Department Design 9.11.-13.11.2020 
+von Tom-Lucas Säger tlsaeger.de  */
+
+let poseNet;
+let video;
+let label, lableCheck;
+
+
+function preload() {
+  poseNet = ml5.poseNet(video,modelReady);
+  console.log(poseNet);
 }
 
-function draw() {
-  background(220);
+function setup() {
+  createCanvas(400, 400);
+  video = createCapture(VIDEO);
+  video.size(400,400);
+  video.hide();
+  classifyVideo();
+
+}
+function draw(){
+console.log(lableCheck)
+background(255)
+image(video, 0,0);
+text(label, width / 2, height -20);
+if(lableCheck == 'Tommy'){
+  image(robot,0,0,width, height)
+}
+}
+
+function modelReady() {
+  createDiv("Model ist geladen")
+  console.log("Model Geladen");
+}
+
+function classifyVideo() {
+  flippedVideo = ml5.flipImage(video);
+  classifier.classify(flippedVideo, gotResult);
+  flippedVideo.remove();
+
+}
+
+// A function to run when we get any errors and the results
+function gotResult(error, results) {
+  // Display error in the console
+  if (error) {
+    console.error(error);
+  }
+  // The results are in an array ordered by confidence.
+  label = 'Label: ' + results[0].label + '\n' + 'Confidence: ' + results[0].confidence;
+  lableCheck = results[0].label;
+  classifyVideo();
+
 }
